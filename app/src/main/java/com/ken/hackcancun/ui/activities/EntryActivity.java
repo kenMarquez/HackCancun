2cf56c0dee22d1f1494313d2ff7019e94d4a26fe
KenMarquez <darius_dam@hotmail.com>
2016-06-20 22:45:57 -0500

    Add login and navigation view panel

--- app/src/main/java/com/ken/hackcancun/ui/activities/EntryActivity.java
+++ app/src/main/java/com/ken/hackcancun/ui/activities/EntryActivity.java
@@ -1,19 +1,34 @@
 package com.ken.hackcancun.ui.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.inputmethod.InputMethodManager;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.ken.hackcancun.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class EntryActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
         hideKeyboard();
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.button_enter)
+    public void enter() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 
     /**
