b35421759f8f512966ee2b7fc8c8daf3bf8b69a4
KenMarquez <darius_dam@hotmail.com>
2016-06-21 00:56:16 -0500

    Add profile section add school field add incom and exit field add percents fields

--- app/src/main/java/com/ken/hackcancun/ui/activities/MainActivity.java
+++ app/src/main/java/com/ken/hackcancun/ui/activities/MainActivity.java
@@ -1,18 +1,36 @@
 package com.ken.hackcancun.ui.activities;
 
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ken.hackcancun.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final Handler mDrawerActionHandler = new Handler();
+    @BindView(R.id.drawer)
+    public DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    //start title drawer
+    private int mNavItemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        ButterKnife.bind(this);
+    }
 
 
 }
